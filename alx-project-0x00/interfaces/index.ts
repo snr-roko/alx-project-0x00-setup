243ab12a4655828deedbb8ee4497d90c6f5a0b81
snr-roko <rbbagyei@outlook.com>
2025-06-01 19:41:38 +0000

    Add Button component and integrate into Landing page

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,15 @@
 export interface PillProps {
   title: string
 }
+
+export interface StyleProps {
+  size: 'sm' | 'md' | 'lg';
+  color: string;
+  backgroundColor: string;
+  roundness: string;
+}
+
+export interface ButtonProps {
+  title: string;
+  styles: StyleProps
+}
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,39 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
+import { StyleProps } from "@/interfaces"
 
 const Landing: React.FC = () => {
+
+  const smallButtonStyle: StyleProps = {
+    size: 'sm',
+    color: 'white-500',
+    backgroundColor: 'blue-500',
+    roundness: 'md'
+  }
+
+  const mediumButtonStyle: StyleProps = {
+    size: 'md',
+    color: 'green-500',
+    backgroundColor: 'white',
+    roundness: 'sm'
+  }
+
+  const largeButtonStyle: StyleProps = {
+    size: 'lg',
+    color: 'white',
+    backgroundColor: 'red-500',
+    roundness: 'full'
+  }
+
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
+      <div className="flex gap-5 justify-center">
+      <Button title="Small" styles={smallButtonStyle} />
+      <Button title="Medium" styles={mediumButtonStyle} />
+      <Button title="Large" styles={largeButtonStyle} />
+      </div>
     </div>
   )
 }
